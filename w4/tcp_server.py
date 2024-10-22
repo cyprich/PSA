af1a0172480eb292cdd888a87caaf99c59248ea9
cyprich <cypooriginal@gmail.com>
2024-10-22 09:29:16 +0200

    w5 multithreading

--- w4/tcp_server.py
+++ w4/tcp_server.py
@@ -1,11 +1,30 @@
 import socket
 from tcp_protocol import CHAT_PROTOCOL, CONTROL_INFO
+from threading import Thread
 
 
 IP = "0.0.0.0"
 PORT = 9999
 USERS = []
 
+
+# obsluhovanie jedneho klienta
+def handle_client(client_sock):
+    while True:
+        buffer = client_sock.recv(1000)
+
+        # (control, data) = protocol.parse_msg(buffer, USERS)
+        parsed_message = protocol.parse_msg(buffer, USERS)
+        control = parsed_message[0]
+
+        if control == CONTROL_INFO.LOGOUT:
+            client_sock.close()
+            break
+
+        elif control == CONTROL_INFO.USERS:
+            users = parsed_message[1]
+            client_sock.send(users.__str__().encode())
+            continue
 
 
 if __name__ == "__main__":
@@ -21,21 +40,7 @@ if __name__ == "__main__":
         print(f"Client connected {client_addr}:{client_port}")
         # print(f"Client connected {}:{}".format(client_addr, client_port))
 
-        # obsluhovanie jedneho klienta
-        while True:
-            buffer = client_sock.recv(1000)
-
-            # (control, data) = protocol.parse_msg(buffer, USERS)
-            parsed_message = protocol.parse_msg(buffer, USERS)
-            control = parsed_message[0]
-
-            if control == CONTROL_INFO.LOGOUT:
-                client_sock.close()
-                break
-
-            elif control == CONTROL_INFO.USERS:
-                users = parsed_message[1]
-                client_sock.send(users.__str__().encode())
-                continue
+        thread = Thread(target=handle_client, args=(client_sock,))
+        thread.start()
 
     sock.close()
